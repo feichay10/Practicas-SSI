409844562c721bda60435e28c345c512ba17e4a3
feichay10 <alu0101364544@ull.edu.es>
2023-12-07 18:58:16 +0000

    Modifying script

--- Practica_10_Gestion_Contraseñas/gen_users_pwgen.sh
+++ Practica_10_Gestion_Contraseñas/gen_users_pwgen.sh
@@ -4,41 +4,67 @@ USERS=("usuario1" "usuario2" "usuario3" "usuario4" "usuario5")
 
 PASSWORDS=()
 
-# Comprobar si los usuarios ya estan creados
-for user in ${USERS[@]}; do
-  if id -u $user >/dev/null 2>&1; then
-    echo "El usuario $user ya existe"
-    continue
+# Comprobar si existen los usuarios que estan en el array USERS,
+# y si existen, cambiar las contrasenias, si existen crear los usuarios
+for user in "${USERS[@]}"; do
+  if id -u "$user" >/dev/null 2>&1; then
+    echo "El usuario $user existe"
+    if [ $user == "usuario1" ]; then
+      password1="123456789"
+      password1_md5=$(echo $password1 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password1" | sudo chpasswd
+      PASSWORDS+=("$password1")
+    elif [ $user == "usuario2" ]; then
+      password2="usuario2"
+      password2_md5=$(echo $password2 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password2" | sudo chpasswd
+      PASSWORDS+=("$password2")
+    elif [ $user == "usuario3" ]; then
+      password3="UsuArio3"
+      password3_md5=$(echo $password3 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password3" | sudo chpasswd
+      PASSWORDS+=("$password3")
+    elif [ $user == "usuario4" ]; then
+      password4="_User@4_"
+      password4_md5=$(echo $password4 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password4" | sudo chpasswd
+      PASSWORDS+=("$password4")
+    elif [ $user == "usuario5" ]; then
+      password5=$(pwgen  -y -A 8 -1)
+      password5_md5=$(echo $password5 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password5" | sudo chpasswd
+      PASSWORDS+=("$password5")
+    fi
   else
-    echo "El usuario $user no existe"
     sudo useradd $user
+    if [ $user == "usuario1" ]; then
+      password1="123456789"
+      password1_md5=$(echo $password1 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password1" | sudo chpasswd
+      PASSWORDS+=("$password1")
+    elif [ $user == "usuario2" ]; then
+      password2="usuario2"
+      password2_md5=$(echo $password2 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password2" | sudo chpasswd
+      PASSWORDS+=("$password2")
+    elif [ $user == "usuario3" ]; then
+      password3="UsuArio3"
+      password3_md5=$(echo $password3 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password3" | sudo chpasswd
+      PASSWORDS+=("$password3")
+    elif [ $user == "usuario4" ]; then
+      password4="_User@4_"
+      password4_md5=$(echo $password4 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password4" | sudo chpasswd
+      PASSWORDS+=("$password4")
+    elif [ $user == "usuario5" ]; then
+      password5=$(pwgen  -y -A 8 -1)
+      password5_md5=$(echo $password5 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+      echo "$user:$password5" | sudo chpasswd
+      PASSWORDS+=("$password5")
+    fi
   fi
 done
-
-password1=$(pwgen -0 8 -A -1)
-password1_md5=$(echo $password1 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
-echo "usuario1:$password1_md" | sudo chpasswd
-PASSWORDS+=("$password1_md")
-
-password2=$(pwgen -n -1 10)
-password2_md5=$(echo $password2 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
-echo "usuario2:$password2_md" | sudo chpasswd
-PASSWORDS+=("$password2_md")
-
-password3=$(pwgen -y -A 8 -1)
-password3_md5=$(echo $password3 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
-echo "usuario3:$password3_md5" | sudo chpasswd
-PASSWORDS+=("$password3_md5")
-
-password4=$(pwgen -c -n -y 10 -1)
-password4_md5=$(echo $password4 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
-echo "usuario4:$password4_md5" | sudo chpasswd
-PASSWORDS+=("$password4_md5")
-
-password5=$(pwgen -c -n -s -B -1 16)
-password5_md5=$(echo $password5 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
-echo "usuario5:$password5_md5" | sudo chpasswd
-PASSWORDS+=("$password5_md5")
 
 echo "Usuarios y contrasenias creados:"
 echo "--------------------------------"
