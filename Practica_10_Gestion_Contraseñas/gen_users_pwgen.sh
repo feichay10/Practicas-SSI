9be148cc1f0ac04fc7d993b4336b7192d46ab6b1
feichay10 <alu0101364544@ull.edu.es>
2023-12-07 01:57:53 +0000

    minor fixes

--- Practica_10_Gestion_Contraseñas/gen_users_pwgen.sh
+++ Practica_10_Gestion_Contraseñas/gen_users_pwgen.sh
@@ -4,30 +4,41 @@ USERS=("usuario1" "usuario2" "usuario3" "usuario4" "usuario5")
 
 PASSWORDS=()
 
-sudo adduser usuario1
+# Comprobar si los usuarios ya estan creados
+for user in ${USERS[@]}; do
+  if id -u $user >/dev/null 2>&1; then
+    echo "El usuario $user ya existe"
+    continue
+  else
+    echo "El usuario $user no existe"
+    sudo useradd $user
+  fi
+done
+
 password1=$(pwgen -0 8 -A -1)
-echo "usuario1:$password1" | sudo chpasswd
-PASSWORDS+=("$password1")
+password1_md5=$(echo $password1 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+echo "usuario1:$password1_md" | sudo chpasswd
+PASSWORDS+=("$password1_md")
 
-sudo adduser usuario2
 password2=$(pwgen -n -1 10)
-echo "usuario2:$password2" | sudo chpasswd
-PASSWORDS+=("$password2")
+password2_md5=$(echo $password2 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+echo "usuario2:$password2_md" | sudo chpasswd
+PASSWORDS+=("$password2_md")
 
-sudo adduser usuario3
 password3=$(pwgen -y -A 8 -1)
-echo "usuario3:$password3" | sudo chpasswd
-PASSWORDS+=("$password3")
+password3_md5=$(echo $password3 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+echo "usuario3:$password3_md5" | sudo chpasswd
+PASSWORDS+=("$password3_md5")
 
-sudo adduser usuario4
 password4=$(pwgen -c -n -y 10 -1)
-echo "usuario4:$password4" | sudo chpasswd
-PASSWORDS+=("$password4")
+password4_md5=$(echo $password4 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+echo "usuario4:$password4_md5" | sudo chpasswd
+PASSWORDS+=("$password4_md5")
 
-sudo adduser usuario5
 password5=$(pwgen -c -n -s -B -1 16)
-echo "usuario5:$password5" | sudo chpasswd
-PASSWORDS+=("$password5")
+password5_md5=$(echo $password5 | makepasswd --clearfrom=- --crypt-md5 | awk '{ print $2 }')
+echo "usuario5:$password5_md5" | sudo chpasswd
+PASSWORDS+=("$password5_md5")
 
 echo "Usuarios y contrasenias creados:"
 echo "--------------------------------"
